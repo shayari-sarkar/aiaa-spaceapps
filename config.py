ecf29d923e16cfcd39acb816f190c0f93141bfec
Shayari Sarkar <85651240+shayari-sarkar@users.noreply.github.com>
2024-10-08 20:10:58 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,1 @@
-import os
 
-class Config(object):
-
-    RECAPTCHA_PUBLIC_KEY = os.environ.get('6Lc_IvspAAAAAJfqdlCwYHcLRp-hCM0E4VzvZ_QH')
-    RECAPTCHA_PRIVATE_KEY = os.environ.get('6Lc_IvspAAAAAMFzhw-zZP_UsQJ0_dGHyOZed9eG')
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess' 
